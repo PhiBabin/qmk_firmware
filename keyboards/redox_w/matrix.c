7a989ebcbf5655768d1852cd0fc98d94ae177924
Philippe Babin <philippe.babin@gmail.com>
2023-10-07 12:41:09 -0400

    Revert volume keys press

--- keyboards/redox_w/matrix.c
+++ keyboards/redox_w/matrix.c
@@ -80,21 +80,13 @@ bool matrix_scan_custom(matrix_row_t current_matrix[]) {
                 const uint16_t diff = (uint16_t)new_value - (uint16_t)old_value;
                 print("\n");
                 if (diff <= 256/2) {
-                    // tap_code(KC_VOLU);
+                    tap_code(KC_VOLU);
                     dprintf("+1 => %d", diff);
                 } else {
-                    // tap_code(KC_VOLD);
+                    tap_code(KC_VOLD);
                     dprintf("-1 => %d", diff);
                 }
                 print("\n");
-                // if (new_value < encoder_values[i])
-                // {
-                //     tap_code(KC_VOLD);
-                // }
-                // else
-                // {
-                //     tap_code(KC_VOLU);
-                // }
             }
         }
     }
